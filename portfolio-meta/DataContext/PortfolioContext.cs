13a4c3e9e867268395b895e1ce1e568f98fb78b8
Kenneth Gorking <kgorking@outlook.com>
2025-04-28 14:19:05 +0200

    Første commit.
    Docker virker, yay.

---
+++ portfolio-meta/DataContext/PortfolioContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataContext
+{
+    public class PortfolioContext : DbContext
+    {
+        public PortfolioContext(DbContextOptions<PortfolioContext> options) : base(options) { }
+
+        public async Task<bool> TestConnectionAsync()
+        {
+            try
+            {
+                await this.Database.OpenConnectionAsync();
+                await this.Database.CloseConnectionAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Database connection failed: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
---
+++ portfolio-meta/Program.cs
@@ -0,0 +1,45 @@
+using DataContext;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+var test = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<PortfolioContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<PortfolioContext>();
+
+if (await context.TestConnectionAsync())
+{
+    Console.WriteLine("Database connection successful.");
+}
+else
+{
+    Console.WriteLine("Database connection failed.");
+}
+
+app.Run();
