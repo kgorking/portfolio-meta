0680b8c1e1e510ba76d95f9d202452f1e100f405
Kenneth Gorking <kgorking@outlook.com>
2025-04-30 10:25:09 +0200

    Added 'Entry' model

---
+++ portfolio-meta/Models/Entry.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Entry
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Extract is required.")]
+
+        public string? Extract { get; set; }
+
+        [Required(ErrorMessage = "Content is required.")]
+        public string? Content { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ portfolio-test/TestDbEntry.cs
@@ -0,0 +1,59 @@
+using DataContext;
+using Models;
+
+namespace portfolio_test;
+
+[TestClass]
+public class TestDbEntry
+{
+    [TestMethod]
+    public void TestCRUD()
+    {
+        using var ctx = new PortfolioContext(Config.DbOptions);
+        var entry = new Entry
+        {
+            Title = "title test",
+            Extract = "a test of the entry model",
+            Content = "123456abcdef",
+            Created = DateTime.Now,
+            LastUpdated = DateTime.Now
+        };
+
+        // C
+        ctx.Entries.Add(entry);
+        Assert.AreEqual(1, ctx.SaveChanges());
+
+        // R
+        var readEntry = ctx.Entries.Where(e => e.ID == entry.ID).Single();
+        Assert.AreEqual(entry, readEntry);
+
+        // U
+        ctx.Entries.Update(readEntry);
+        readEntry.Content = "[redacted]";
+        Assert.AreEqual(1, ctx.SaveChanges());
+
+        var updatedEntry = ctx.Entries.Where(e => e.ID == entry.ID).Single();
+        Assert.AreEqual(readEntry.Content, updatedEntry.Content);
+
+        // D
+        ctx.Entries.Remove(entry);
+        Assert.AreEqual(1, ctx.SaveChanges());
+    }
+
+    [TestMethod]
+    public void TestRequirements()
+    {
+        using var ctx = new PortfolioContext(Config.DbOptions);
+        var entry = new Entry
+        {
+            Title = null,
+            Extract = null,
+            Content = null,
+            Created = DateTime.Now,
+            LastUpdated = DateTime.Now
+        };
+
+        ctx.Entries.Add(entry);
+        Assert.ThrowsException<Microsoft.EntityFrameworkCore.DbUpdateException>(() => ctx.SaveChanges());
+    }
+}
